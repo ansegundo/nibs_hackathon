6080145beccb44b10dbd2c09c2d65019ce6718dd
Matheus Almeida <mat.almeida@live.com>
2017-05-18 17:39:48 -0300

    pagina feed n funciona, possivelmente por conta de alguma chamada assincrona

--- js/pseudo_spa.js
+++ js/pseudo_spa.js
@@ -1,8 +1,25 @@
-function page_test() {
-    let page = document.querySelector(".content");
+function page_feed() {
+    let page = document.querySelector("#page1");
 
-    page.innerHTML = `
-    <h1>Pau no seu cu</h1>
-    `
+    page.innerHTML = ``;
+
+    $("#page1").load("feed.html");
+
+    $(document).ready(function() {
+        $('ul.tabs').tabs();
+    });
+
+    loadCards();
 }
 
+function page_map() {
+    let page = document.querySelector("#page1");
+
+    page.innerHTML = '';
+
+    $("#page1").load("map.html");
+
+    $(document).ready(function() {
+        $('ul.tabs').tabs();
+    });
+}
