980af31fe4ed5cb95b29bdaafcc6f837097be0c0
Matheus Almeida <mat.almeida@live.com>
2017-05-18 05:27:04 -0300

    função cards

--- js/pseudo_spa.js
+++ js/pseudo_spa.js
@@ -7,16 +7,23 @@ function page_test() {
 }
 
 function cria_cards(name, description) {
-    `
+    let pessoas = document.querySelector("#pessoas");
+    let div = document.createElement("div");
+
+    let template = `
     <div class="row">
         <div class="col s12 m6">
-          <div class="card default-primary-color">
+          <div class="card red">
             <div class="card-content white-text">
-              <span class="card-title">${name}Card Title</span>
+              <span class="card-title">${name}</span>
               <p>${description}.</p>
             </div>
           </div>
         </div>
       </div>
-    `
+    `;
+
+    div.innerHTML = template;
+
+    pessoas.appendChild(div);
 }
